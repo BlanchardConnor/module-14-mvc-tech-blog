369725397d476c18415aded8f71f96b0e887d574
BlanchardConnor <BlanchardConnor73@gmail.com>
2023-08-03 18:35:00 -0500

    added utils and started the comment-routes api

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const sequelize = require('../config/connection');
+const { Comment } = require('../models');
+const withAuth = require('../utils/auth');
+router.get('/', withAuth, (req, res) => {
+    Comment.findAll({})
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+router.get('/:id', withAuth, (req, res) => {
+    Comment.findAll({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+router.post('/', withAuth, (req, res) => {
+    if(req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id,
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    }
+});
+
+
+   
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+          router.get("/new", (req, res) => {
+            res.render("new-post");
+          });
+          
+module.exports = router;
