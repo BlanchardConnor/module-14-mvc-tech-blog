022d9bdcf7b282056c1723f7f28369e3da013867
BlanchardConnor <BlanchardConnor73@gmail.com>
2023-08-04 13:51:09 -0500

    created and completed post-routes.js. formatted comment and dashboard routes. other files need formatting

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,11 +4,11 @@ const { Comment } = require('../models');
 const withAuth = require('../utils/auth');
 router.get('/', withAuth, (req, res) => {
     Comment.findAll({})
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
 router.get('/:id', withAuth, (req, res) => {
@@ -17,36 +17,67 @@ router.get('/:id', withAuth, (req, res) => {
             id: req.params.id
         }
     })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
 router.post('/', withAuth, (req, res) => {
-    if(req.session) {
+    if (req.session) {
         Comment.create({
             comment_text: req.body.comment_text,
             post_id: req.body.post_id,
             user_id: req.session.user_id,
         })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
     }
 });
 
-
-   
+router.put('/:id', withAuth, (req, res) => {
+    Comment.update({
+        comment_text: req.body.comment_text,
+    },
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
         .catch((err) => {
             console.log(err);
             res.status(500).json(err);
-          });
-          router.get("/new", (req, res) => {
-            res.render("new-post");
-          });
-          
+        });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
